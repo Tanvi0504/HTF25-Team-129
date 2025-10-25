8c6565abc281b78d0ad0d01183574fd16fdd2581
Tanvi0504 <seelamtanvi@gmail.com>
2025-10-25 23:11:12 +0530

    updated

--- backend/models.py
+++ backend/models.py
@@ -52,3 +52,4 @@ class Feedback(Base):
     rating = Column(Integer)
     comments = Column(Text, nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
